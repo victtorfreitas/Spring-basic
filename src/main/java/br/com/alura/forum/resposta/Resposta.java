144411101a809babb6b27ae37f62c1857966a43c
Victtor Freitas <victtorfreitas95@gmail.com>
2020-02-18 00:15:45 -0300

    Relaciona Entidade Resposta

--- src/main/java/br/com/alura/forum/resposta/Resposta.java
+++ src/main/java/br/com/alura/forum/resposta/Resposta.java
@@ -4,15 +4,22 @@ import br.com.alura.forum.topico.Topico;
 import br.com.alura.forum.usuario.Usuario;
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class Resposta {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String mensagem;
+
+    @ManyToOne
     private Topico topico;
     private LocalDateTime dataCriacao;
+
+    @ManyToOne
     private Usuario autor;
     private Boolean solucao;
 

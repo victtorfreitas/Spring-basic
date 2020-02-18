254ae82766e990e608a2462e47a73313c7e4709e
Victtor Freitas <victtorfreitas95@gmail.com>
2020-02-18 00:17:40 -0300

    Adiciona vinculo a entidade Usuario

--- src/main/java/br/com/alura/forum/usuario/Usuario.java
+++ src/main/java/br/com/alura/forum/usuario/Usuario.java
@@ -2,8 +2,16 @@ package br.com.alura.forum.usuario;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
